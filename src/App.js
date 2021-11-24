cb477ff3f7e634dc453238eadf97c512f6816da7
Sanyamk0909 <63477713+Sanyamk0909@users.noreply.github.com>
2021-11-24 23:10:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+ 
+import React,{Component} from 'react';
+
+class App extends Component {
+  
+
+    state = {
+      selectedFile: null
+    };
+    
+    
+    onFileChange = event => {
+    
+      // Update the state
+      this.setState({ selectedFile: event.target.files[0] });
+    
+    };
+    
+   
+    onFileUpload = () => {
+    
+      // Create an object of formData
+      const formData = new FormData();
+    
+      // Update the formData object
+      formData.append(
+        "myFile",
+        this.state.selectedFile,
+        this.state.selectedFile.name
+      );
+
+
+      // Details of the uploaded file
+      console.log(this.state.selectedFile);
+    
+      // Request made to the backend api
+      // Send formData object
+      axios.post("api/uploadfile", formData);
+    };
+    
+    // File content to be displayed after
+    // file upload is complete
+    fileData = () => {
+   
+    
+      if (this.state.selectedFile) {
+         
+        return (
+          <div>
+            <h2>File Details:</h2>
+             
+<p>File Name: {this.state.selectedFile.name}</p>
+ 
+             
+<p>File Type: {this.state.selectedFile.type}</p>
+ 
+
+ 
+          </div>
+          
+        );
+      } else {
+        return (
+          <div>
+            <br />
+            <h4>Choose before Pressing the Upload button</h4>
+          </div>
+        );
+      }
+    };
+    
+      
+    
+  
+    
+    render() {
+    
+      return (
+        <div>
+            <h1>
+              Saarthi
+            </h1>
+            <h3>
+              Upload your music files! :)
+            </h3>
+            <div>
+                <input type="file" onChange={this.onFileChange} />
+                <button onClick={this.onFileUpload}>
+                  Upload!
+                </button>
+            </div>
+            {this.fileData()}
+          
+  
+        </div>
+        
+      );
+    }
+  }
+ 
+  export default App;
+  //
